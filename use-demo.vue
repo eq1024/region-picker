277f9827151d364bbdc575cfc2bc2b7929327ffe
goodman <zhouji@l-helper.com>
2022-11-07 17:07:14 +0800

    init

---
+++ use-demo.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <locate-picker @change="change">
+      <span>{{ locateValue }}</span>
+    </locate-picker>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      locateValue: '请选择地区',
+    };
+  },
+  methods: {
+    change(data) {
+      this.locateValue = data.data.join(' ');
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
